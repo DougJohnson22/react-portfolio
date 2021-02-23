52838c6763624a09b447c5e54b553bbd820bd146
Douglas Johnson <trunks22a@gmail.com>
2021-02-22 17:54:17 -0800

    NavBar Styling

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./style.css"
 import { Link, useLocation } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
@@ -6,7 +7,6 @@ import Nav from "react-bootstrap/Nav"
 
 export default function NavBar() {
     const location = useLocation();
-
 
     return (
         <Navbar bg="light" expand="lg">
@@ -23,14 +23,14 @@ export default function NavBar() {
 
                     <Nav.Link>
                         <Link to="/history" className={location.pathname === "/history" ? "nav-link active" : "nav-link"}>
-                        History
+                            History
                         </Link>
                     </Nav.Link>
 
                     <Nav.Link>
                         <Link to="/portfolio" className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
                             Portfolio
-                        </Link> 
+                        </Link>
                     </Nav.Link>
 
                     <Nav.Link>
@@ -45,33 +45,45 @@ export default function NavBar() {
             {/* Socials */}
             <Navbar.Collapse className="justify-content-end">
 
-                <Navbar.Brand href="https://github.com/DougJohnson22">
-                    <img
-                        src="https://i.postimg.cc/QMg6qPnr/iconfinder-142-Github-logo-logos-4373152.png"
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-                        alt="GitHub Link"
-                    />
-                </Navbar.Brand>
-                <Navbar.Brand href="https://www.linkedin.com/in/douglas-johnson-942ba2200/">
-                    <img
-                        src="https://i.postimg.cc/tgD3JHM7/iconfinder-Rounded-Linkedin2-svg-5282542.png"
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-                        alt="LinkedIn Link"
-                    />
-                </Navbar.Brand>
-                <Navbar.Brand href="https://www.instagram.com/conjurationwizard/">
-                    <img
-                        src="https://i.postimg.cc/6Q1hzXLg/iconfinder-38-instagram-1161953.png"
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-                        alt="Instagram Link"
-                    />
-                </Navbar.Brand>
+                <div class="box-wrap">
+                    <div class="box">
+                        <Navbar.Brand href="https://github.com/DougJohnson22">
+                            <img
+                                src="https://i.postimg.cc/QMg6qPnr/iconfinder-142-Github-logo-logos-4373152.png"
+                                width="30"
+                                height="30"
+                                id="socialIcon"
+                                className="d-inline-block align-top"
+                                alt="GitHub Link"
+                            />
+                        </Navbar.Brand>
+                    </div>
+                    <div class="box">
+                        <Navbar.Brand href="https://www.linkedin.com/in/douglas-johnson-942ba2200/">
+                            <img
+                                src="https://i.postimg.cc/tgD3JHM7/iconfinder-Rounded-Linkedin2-svg-5282542.png"
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                                id="socialIcon"
+                                alt="LinkedIn Link"
+                            />
+                        </Navbar.Brand>
+                    </div>
+                    <div class="box">
+                        <Navbar.Brand href="https://www.instagram.com/conjurationwizard/">
+                            <img
+                                src="https://i.postimg.cc/6Q1hzXLg/iconfinder-38-instagram-1161953.png"
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                                id="socialIcon"
+                                alt="Instagram Link"
+                            />
+                        </Navbar.Brand>
+                    </div>
+                </div>
+                
             </Navbar.Collapse>
 
 
